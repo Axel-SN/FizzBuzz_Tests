06ae5b7ad24804a0a4963beb066e727a6364496f
Axel-SN <137889141+Axel-SN@users.noreply.github.com>
2025-02-27 18:31:45 +0100

    Created fizzbuzz_tests
    Created fizzbuzz_tests to try a coding challenge.

---
+++ fizzbuzz_tests.js
@@ -0,0 +1,25 @@
+///////// fizzbuzz tests ////////
+
+let logOut1 = "for i=";
+let logOut2 = " -> ";
+let logOut3 = "";
+
+for (let i = 0; i < 31; i++) {
+  logOut3 = "";
+  if (i % 3 == 0 && i % 5 == 0) {
+    logOut3 = "fizzbuzz";
+  } else if (i % 3 == 0) {
+    logOut3 = "fizz";
+  } else if (i % 5 == 0) {
+    logOut3 = "buzz";
+  } else {
+    logOut3 = i;
+  }
+  if (i % 6 == 0) {
+    logOut3 += "brat";
+  }
+  if (i % 9 == 0) {
+    logOut3 += "wurst";
+  }
+  console.log(logOut1 + i + logOut2 + logOut3);
+}
